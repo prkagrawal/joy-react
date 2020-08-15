d925cb8c47a6136370ab1c0b31811d7bfa3210fb
Prince Agrawal <princeagrawalnew123@gmail.com>
2020-08-15 19:48:24 +0530

    Updates

---
+++ precache-manifest.b178f40803cbca20846bcdf6d02bf9eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e27a48c5736f6cc7302b792655114f4",
+    "url": "/joy-react/index.html"
+  },
+  {
+    "revision": "4932e5414031fad3e882",
+    "url": "/joy-react/static/css/main.74309904.chunk.css"
+  },
+  {
+    "revision": "822f76c223daa37dc0d2",
+    "url": "/joy-react/static/js/2.77b29d28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/joy-react/static/js/2.77b29d28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4932e5414031fad3e882",
+    "url": "/joy-react/static/js/main.453cb77e.chunk.js"
+  },
+  {
+    "revision": "d5dc102d380078cfef48",
+    "url": "/joy-react/static/js/runtime-main.39aed7bf.js"
+  },
+  {
+    "revision": "b0849a613b2dd377ac784e71003c8ee0",
+    "url": "/joy-react/static/media/b1.b0849a61.png"
+  }
+]);
